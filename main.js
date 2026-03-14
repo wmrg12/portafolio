9d386ada38cd92bdc7a9b6a82843ffd0207c68b0
Vicente Guzman Aliza Abigail <alizaabigailvicenteguzman@gmail.com>
2026-03-13 20:28:48 -0400

    Eliminación de main.js por refactorización del proyecto

--- main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("cv").onclick = function(){
-    alert("Descargando CV...");
-    }
-    
-    document.getElementById("contact").onclick = function(){
-    alert("Abriendo contacto...");
-    }
